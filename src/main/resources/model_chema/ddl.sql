c96698f572196f43371a06dea8869c31ee2abb06
WhiteN <whiten.ncs@gmail.com>
2020-05-02 18:26:16 +0300

    add case_types table

--- src/main/resources/model_chema/ddl.sql
+++ src/main/resources/model_chema/ddl.sql
@@ -1,29 +1,18 @@
-CREATE TABLE public.covid19 (
+CREATE TABLE cases (
 	id serial NOT NULL,
 	country_region varchar(100) NOT NULL,
 	last_update timestamp NOT NULL,
 	confirmed int4 NOT NULL,
 	deaths int4 NOT NULL,
 	recovered int4 NOT NULL,
-	source_update timestamp NOT NULL,
-	CONSTRAINT covid19_pkey PRIMARY KEY (id)
+	CONSTRAINT cases_pkey PRIMARY KEY (id)
 );
-CREATE INDEX idx_confirmed_deaths_recovered ON public.covid19 USING btree (confirmed, deaths, recovered);
-CREATE INDEX lower_province_state_country_region ON public.covid19 USING btree (lower((country_region)::text));
-CREATE INDEX time_last_update_last_update ON public.covid19 USING btree (last_update, source_update);
+CREATE INDEX idx_confirmed_deaths_recovered ON cases USING btree (confirmed, deaths, recovered);
+CREATE INDEX lower_province_state_country_region ON cases USING btree (lower((country_region)::text));
+CREATE INDEX time_last_update_last_update ON cases USING btree (last_update, source_update);
 
-CREATE OR REPLACE FUNCTION public.source_update()
- RETURNS trigger
- LANGUAGE plpgsql
-AS $function$
-    BEGIN
-       NEW.source_update := current_timestamp;
-       RETURN NEW;
-    END;
-$function$;
-
-
-CREATE TRIGGER inset_row_to_source_update BEFORE
-insert on public.covid19
-	FOR EACH row
-    EXECUTE PROCEDURE source_update();
+CREATE TABLE case_types (
+	id serial NOT NULL,
+    name varchar(100) NOT NULL,
+	CONSTRAINT case_types_pkey PRIMARY KEY (id)
+);
